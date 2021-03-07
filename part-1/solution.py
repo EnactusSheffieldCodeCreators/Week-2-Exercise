fac5c6950ff3cc6249aeb9c9260ccabbc637c692
Sam_The_Dude <kent_sam@outlook.com>
2021-03-07 21:12:26 +0000

    Added a first attempt at the part 1.

---
+++ part-1/solution.py
@@ -0,0 +1,26 @@
+# Solution to part 1.
+
+# ==== Imports =====
+from youtube_transcript_api import YouTubeTranscriptApi # pip install youtube_transcript_api
+import re
+
+# ===== Constants =====
+VIDEO_ID = "alTRvtmWi7k"
+SEARCH_WORDS = ["um", "uh"]
+
+def count_words(words, text):
+    count = 0
+    for word in words:
+        pattern = "(?:^|\W)" + word + "(?:^|\W)" # Regex pattern matching
+        matches = re.findall(pattern, text, re.IGNORECASE)
+        count += len(matches)
+    return count
+    
+
+video_transcript = YouTubeTranscriptApi.get_transcript(VIDEO_ID)
+um_uh_counter = 0
+
+for section in video_transcript: # Transcript is separated into sections.
+    um_uh_counter += count_words(SEARCH_WORDS, section['text'])
+
+print("Counted {} \"ums\" or \"uhs\"".format(um_uh_counter))
